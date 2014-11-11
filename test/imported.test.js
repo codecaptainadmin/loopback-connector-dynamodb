3fba5a2c3e2b447783547310c6b3972d8d3a97cc
huangxin0921 <huangxin@cs.unc.edu>
2014-11-10 18:05:55 -0800

    Initial implementation of Create interface and unit testing

--- test/imported.test.js
+++ test/imported.test.js
@@ -1,7 +1,33 @@
 describe('dynamodb imported features', function () {
+  var Post, db;
+
   before(function () {
     require('./init.js');
+    db = getDataSource();
+
+    Post = db.define('Post', {
+      title: { type: String, length: 255, index: true },
+      content: { type: String },
+      comments: [String]
+    });
   });
-  
+
+  beforeEach(function (done) {
+    done();
+  });
+
+  it('should create', function (done) {
+    console.log("Create Test");
+    Post.create({title: 'Post', content: 'Post content', comments: ["nice", "ooo"]}, function (err, post) {
+      console.log("Error:", err);
+      done(err);
+    });
+  });
+
+  after(function (done) {
+    done();
+  });
+
   require('loopback-datasource-juggler/test/common.batch.js');
 });
+
